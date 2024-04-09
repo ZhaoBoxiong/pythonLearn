00ffd978700b03fc84b8034170bf69e0e2c3a108
boxiong <zhaoboxiong@xyl.cn>
2024-04-09 12:44:03 +0800

    python code for tutorial day 2

--- MyPython.py
+++ MyPython.py
@@ -1,1 +1,41 @@
 print("Hello Python World")
+
+def python_loop():
+    I_MAX = 3
+    J_MAX = 2
+    for i in range(I_MAX):
+        for j in range(J_MAX):
+            print("value of i is:", i, end=", ")
+            print("value of j is:", j)
+            
+python_loop()
+
+
+num = 10
+pi = 3.14
+letter = 'A'
+
+my_list = [1, 2, 3, 4]
+my_tuple = (1, 2, 3, 4)
+my_dict = {'name': 'Alice', 'age': 25}
+my_set = {1, 2, 3, 4}
+
+print("num =", num)
+print("pi =", pi)
+print("letter =", letter)
+print("my_list =", my_list)
+print("my_tuple =", my_tuple)
+print("my_dict =", my_dict)
+print("my_set =", my_set)
+
+
+my_list = [1, 2, 3, 4, 5]
+my_list.append(6)
+
+for i in range(len(my_list)):
+    print("my_list[{}] = {}".format(i, my_list[i]))
+
+try:
+    x = 10 / 0
+except ZeroDivisionError:
+    print("The divisor cannot be zero.")
---
+++ README.md
@@ -0,0 +1,7 @@
+# day 1
+
+[python零基础入门 （2）-- 从Hello World开始](https://blog.csdn.net/m0_59438914/article/details/137505165?spm=1001.2014.3001.5501)
+
+# day 2
+
+[python零基础入门 （3）-- python语法入门](https://blog.csdn.net/m0_59438914/article/details/137540787?spm=1001.2014.3001.5501)
